57bbabc8c502d5267143ae898e6b9ff0ff261841
Michelle Kim <pasolre95kim@gmail.com>
2018-07-15 02:14:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,14 @@
+def my_collect(collection)
+    new_array = []
+    i = 0
+    while i < collection.length
+      new_array << yieldcollection[i]
+      i+=1
+end
+  new_array
+end
+
+
 def my_collect(array)
   container = []
   counter = 0
